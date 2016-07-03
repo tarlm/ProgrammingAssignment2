1659503b5241ea3d8cc07e33242765ec6bcbd9a4
radouhou <radouhou@free.fr>
2016-07-03 13:56:49 +0200

    Implemented both makeCacheMatrix and cacheSolve functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+## MakeCacheMatrix: cache a matrix object and contrains a list of functions use to
+## set or get matrix or its inverse matrix
+## cacheSolve: calculate the inverse matrix and cached it
 
-## Write a short comment describing this function
+## Set the cached inverse matrix if matrix changed
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    
+    matrix_invers <- NULL
+    set <- function(y) {
+        x <<- y
+        matrix_invers <<- NULL
+    }
+    
+    get <- function() x
+    setmatrixInverse <- function(matrix) matrix_invers <<- matrix
+    getmatrixInverse <- function() matrix_invers
+    list(set = set, get = get,
+         setmatrixInverse = setmatrixInverse,
+         getmatrixInverse = getmatrixInverse)
 }
 
 
-## Write a short comment describing this function
+## Returns inverse matrix if exists in cache or calculate it if not
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    matrix_invers <- x$getmatrixInverse()
+    if(!is.null(matrix_invers)) {
+        message("getting cached data")
+        return(matrix_invers)
+    }
+    
+    data <- x$get()
+    matrix_invers <- solve(data, ...)
+    x$setmatrixInverse(matrix_invers)
+    matrix_invers
 }
+
+###################### Test Space ##############
+# my_matrix <- matrix(data = c(4,2,7,6), nrow = 2, ncol = 2)
+# matObject <- makeCacheMatrix(mat)
+
+# Get Matrix and matrix inverse from object
+# matObject$get()
+# matObject$getmatrixInverse() # NULL
+
+# run cache function 
+# cacheSolve(matObject)
+
+# Get Matrix and matrix inverse from object
+# matObject$get()
+# matObject$getmatrixInverse() # has not null value now
